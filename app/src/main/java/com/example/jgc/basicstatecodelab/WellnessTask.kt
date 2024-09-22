ac62d92d07c510603cd93e0f4d22c73bb65f94da
Jorge Guzmán <s21120196@alumnos.itsur.edu.mx>
2024-09-22 03:37:48 -0600

    Fin

--- app/src/main/java/com/example/jgc/basicstatecodelab/WellnessTask.kt
+++ app/src/main/java/com/example/jgc/basicstatecodelab/WellnessTask.kt
@@ -1,3 +1,12 @@
 package com.example.jgc.basicstatecodelab
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
-data class WellnessTask(val id: Int, val label: String)
+class WellnessTask(
+    val id: Int,
+    val label: String,
+    initialChecked: Boolean = false
+) {
+    var checked by mutableStateOf(initialChecked)
+}
