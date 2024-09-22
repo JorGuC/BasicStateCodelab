b338f026bb7253f5bf3d85666c8543830e4abea5
Jorge Guzmán <s21120196@alumnos.itsur.edu.mx>
2024-09-22 03:00:37 -0600

    Lista con checkBox

--- app/src/main/java/com/example/jgc/basicstatecodelab/WellnessScreen.kt
+++ app/src/main/java/com/example/jgc/basicstatecodelab/WellnessScreen.kt
@@ -2,8 +2,13 @@ package com.example.jgc.basicstatecodelab
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.Column
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    Column(modifier = modifier) {
+        StatefulCounter()
+        WellnessTasksList()
+    }
 }
+
---
+++ app/src/main/java/com/example/jgc/basicstatecodelab/WellnessTask.kt
@@ -0,0 +1,3 @@
+package com.example.jgc.basicstatecodelab
+
+data class WellnessTask(val id: Int, val label: String)
